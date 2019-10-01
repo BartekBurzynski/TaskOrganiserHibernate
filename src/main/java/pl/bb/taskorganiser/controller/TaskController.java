64eecfc3de53820cf6b186a325f3d4d6394f5a76
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-10-01 22:43:34 +0200

    ZadanieOrganizacjaZadanHibernate.

--- src/main/java/pl/bb/taskorganiser/controller/TaskController.java
+++ src/main/java/pl/bb/taskorganiser/controller/TaskController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.bb.taskorganiser.enums.TaskDone;
-import pl.bb.taskorganiser.model.Task;
+import pl.bb.taskorganiser.entity.Task;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
